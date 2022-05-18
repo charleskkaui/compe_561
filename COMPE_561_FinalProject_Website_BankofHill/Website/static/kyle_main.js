584c7634db1401946ac0ab1c64214ba1758dde38
kylebhill <100802585+kylebhill@users.noreply.github.com>
2022-05-18 15:22:08 -0700

    Add files via upload

---
+++ COMPE_561_FinalProject_Website_BankofHill/Website/static/kyle_main.js
@@ -0,0 +1,22 @@
+function changeTab(evt, tabName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+
+$(document).ready(function(){
+    $("select.source").change(function(){
+        var sel = $(this).children("option:selected").val();
+        document.getElementById("sbal").value = document.getElementById(String(sel)).innerText;
+    });
+});
+
